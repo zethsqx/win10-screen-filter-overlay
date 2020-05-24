da16a63ab43ecf138677dfc5c811ebba39e5b161
zethsqx <ohmygoshybrox@gmail.com>
2020-05-25 01:32:08 +0800

    fix text for systray icon added images for readme

--- ScreenFilter/Control.Designer.cs
+++ ScreenFilter/Control.Designer.cs
@@ -90,7 +90,7 @@
             // 
             this.notifyIcon1.ContextMenuStrip = this.contextMenuStrip1;
             this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
-            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Text = "ScreenFilter";
             this.notifyIcon1.Visible = true;
             this.notifyIcon1.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseDoubleClick);
             // 
@@ -108,21 +108,21 @@
             // OpenBtn
             // 
             this.OpenBtn.Name = "OpenBtn";
-            this.OpenBtn.Size = new System.Drawing.Size(360, 48);
+            this.OpenBtn.Size = new System.Drawing.Size(178, 48);
             this.OpenBtn.Text = "Open";
             this.OpenBtn.Click += new System.EventHandler(this.OpenBtn_Click);
             // 
             // AboutBtn
             // 
             this.AboutBtn.Name = "AboutBtn";
-            this.AboutBtn.Size = new System.Drawing.Size(360, 48);
+            this.AboutBtn.Size = new System.Drawing.Size(178, 48);
             this.AboutBtn.Text = "About";
             this.AboutBtn.Click += new System.EventHandler(this.AboutBtn_Click);
             // 
             // CloseBtn
             // 
             this.CloseBtn.Name = "CloseBtn";
-            this.CloseBtn.Size = new System.Drawing.Size(360, 48);
+            this.CloseBtn.Size = new System.Drawing.Size(178, 48);
             this.CloseBtn.Text = "Close";
             this.CloseBtn.Click += new System.EventHandler(this.CloseBtn_Click);
             // 
